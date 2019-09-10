f54ae3d01fcd311191b21285b2cbdfc5ffd95336
Jilin Hu <hujilin1229@163.com>
2019-09-10 20:45:26 +0400

    add h5 dataset processsing

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -5,7 +5,9 @@ import os
 import datetime
 import re
 
-def reshape_patch(img_tensor, patch_size):
+def reshape_patch(img_tensor, patch_size_width, patch_size_height=None):
+    if patch_size_height is None:
+        patch_size_height = patch_size_width
     assert 5 == img_tensor.ndim
     batch_size = np.shape(img_tensor)[0]
     seq_length = np.shape(img_tensor)[1]
@@ -13,33 +15,36 @@ def reshape_patch(img_tensor, patch_size):
     img_width = np.shape(img_tensor)[3]
     num_channels = np.shape(img_tensor)[4]
     a = np.reshape(img_tensor, [batch_size, seq_length,
-                                int(img_height/patch_size), patch_size,
-                                int(img_width/patch_size), patch_size,
+                                int(img_height/patch_size_height), patch_size_height,
+                                int(img_width/patch_size_width), patch_size_width,
                                 num_channels])
 
     b = np.transpose(a, [0,1,2,4,3,5,6])
     patch_tensor = np.reshape(b, [batch_size, seq_length,
-                                  int(img_height/patch_size),
-                                  int(img_width/patch_size),
-                                  patch_size*patch_size*num_channels])
+                                  int(img_height/patch_size_height),
+                                  int(img_width/patch_size_width),
+                                  patch_size_width*patch_size_height*num_channels])
     return patch_tensor
 
-def reshape_patch_back(patch_tensor, patch_size):
+def reshape_patch_back(patch_tensor, patch_size_width, patch_size_height=None):
+    if patch_size_height is None:
+        patch_size_height = patch_size_width
+
     assert 5 == patch_tensor.ndim
     batch_size = np.shape(patch_tensor)[0]
     seq_length = np.shape(patch_tensor)[1]
     patch_height = np.shape(patch_tensor)[2]
     patch_width = np.shape(patch_tensor)[3]
     channels = np.shape(patch_tensor)[4]
-    img_channels = int(channels / (patch_size*patch_size))
+    img_channels = int(channels / (patch_size_width*patch_size_height))
     a = np.reshape(patch_tensor, [batch_size, seq_length,
                                   patch_height, patch_width,
-                                  patch_size, patch_size,
+                                  patch_size_height, patch_size_width,
                                   img_channels])
     b = np.transpose(a, [0,1,2,4,3,5,6])
     img_tensor = np.reshape(b, [batch_size, seq_length,
-                                int(patch_height * patch_size),
-                                int(patch_width * patch_size),
+                                int(patch_height * patch_size_height),
+                                int(patch_width * patch_size_width),
                                 img_channels])
     return img_tensor
 
