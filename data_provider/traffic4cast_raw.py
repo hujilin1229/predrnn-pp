87050f9a953b09f5e0e65f751393c7bac7134b9d
Jilin Hu <hujilin1229@163.com>
2019-09-11 16:09:44 +0400

    fix the h5py IO multiprocess error

--- data_provider/traffic4cast_raw.py
+++ data_provider/traffic4cast_raw.py
@@ -27,7 +27,7 @@ class InputHandle:
     def begin(self, do_shuffle = True):
         # construct dataset
         cache = 6
-        loader_params = {'batch_size': self.num_files, 'shuffle': do_shuffle, 'num_workers': 0}
+        loader_params = {'batch_size': self.num_files, 'shuffle': do_shuffle, 'num_workers': 6}
         dataset = HDF5Dataset(self.paths, recursive=False, load_data=False, data_cache_size=cache)
         self.data_loader = data.DataLoader(dataset, **loader_params)
         self.current_position = 0
