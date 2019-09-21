385955f80761ea7d2aa89589077644450d8a01cd
Jilin_Mac <hujilin1229@163.com>
2019-09-21 15:12:46 +0400

    test heading unique

--- train_h5_capsule.py
+++ train_h5_capsule.py
@@ -249,12 +249,13 @@ def main(argv=None):
         # print("Heading Unique", np.unique(heading_image), flush=True) #[  0.   1.  85. 170. 255.] output
         heading_image = (heading_image // 85).astype(np.int8) + 1
         heading_image[tem_data[:, :, :, :, 2] == 0] = 0
-        print("Heading Unique", np.unique(heading_image), flush=True)
-        heading_image = heading_table[heading_image]
+        # print("Heading Unique", np.unique(heading_image), flush=True)
         # select the corresponding data
         heading_selected = np.zeros_like(heading_image, np.int8)
         heading_selected[heading_image == heading] = heading
         heading_image = heading_selected
+
+        heading_image = heading_table[heading_image]
 
         speed_on_axis = np.expand_dims(imss[:, :, :, :, 1] / np.sqrt(2), axis=-1)
         imss = speed_on_axis * heading_image
