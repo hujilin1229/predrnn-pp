a9bd939d0a32c805758a3791ea7f96b2ce61f829
Jilin Hu <hujilin1229@163.com>
2019-09-27 14:22:31 +0400

    test on all testing data

--- test_h5.py
+++ test_h5.py
@@ -233,7 +233,7 @@ def main(argv=None):
             # data = np.stack(data, axis=0)
             # type casting
 
-            data = data.reshape(-1, FLAGS.input_length+FLAGS.seq_length,
+            data = data.reshape(-1, FLAGS.seq_length,
                                 FLAGS.img_height*FLAGS.patch_size_height, FLAGS.img_width*FLAGS.patch_size_width, 3)
 
             test_dat = data.astype(np.float32) / 255.0
