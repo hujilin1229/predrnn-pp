348cb643bc8af92b825f2de8449bb899d5d6a144
Jilin Hu <hujilin1229@163.com>
2019-09-27 14:38:31 +0400

    test on all testing data

--- test_h5.py
+++ test_h5.py
@@ -229,13 +229,13 @@ def main(argv=None):
         with h5py.File(os.path.join(test_data_paths, f), 'r') as h5_file:
             data = h5_file['array'][()]
             # get relevant training data pieces
-            # data = [data[y - FLAGS.input_length:y + FLAGS.seq_length - FLAGS.input_length] for y in indicies]
-            # data = np.stack(data, axis=0)
+            data = [data[y - FLAGS.input_length:y + FLAGS.seq_length - FLAGS.input_length] for y in indicies]
+            data = np.stack(data, axis=0)
+
+            # all validation data is applied
+            # data = np.reshape(data,(-1, FLAGS.seq_length,
+            #                     FLAGS.img_height*FLAGS.patch_size_height, FLAGS.img_width*FLAGS.patch_size_width, 3))
             # type casting
-
-            data = np.reshape(data,(-1, FLAGS.seq_length,
-                                FLAGS.img_height*FLAGS.patch_size_height, FLAGS.img_width*FLAGS.patch_size_width, 3))
-
             test_dat = data.astype(np.float32) / 255.0
             test_dat = preprocess.reshape_patch(test_dat, FLAGS.patch_size_width, FLAGS.patch_size_height)
             batch_size = data.shape[0]
